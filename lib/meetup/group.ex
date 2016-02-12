10841b2a4ccafb5ec90415cd7af198e97863efb5
Guilherme <ggpasqualino@gmail.com>
2016-02-12 13:27:48 +0100

    Add member endpoint request and extract response handling

--- lib/meetup/group.ex
+++ lib/meetup/group.ex
@@ -1,6 +1,7 @@
 defmodule Meetup.Group do
   @api_key System.get_env("MEETUP_API_KEY")
-  @endpoint "https://api.meetup.com/2/profiles"
+  @group_members_endpoint "https://api.meetup.com/2/profiles"
+  @member_endpoint "https://api.meetup.com/members/"
 
   def members(group, page \\ 20)
   def members(group, page) do
@@ -11,12 +12,32 @@ defmodule Meetup.Group do
       page: page
     }
 
-    @endpoint
+    @group_members_endpoint
     |> HTTPoison.get([], params: params )
-    |> case do
-         {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> Poison.decode!(body)
-         response -> {:error, "#{inspect response}"}
-       end
+    |> handle_response
     |> Map.get("results")
+  end
+
+  def member(group, member_id, extra_fields \\ ["memberships", "topics"])
+  def member(group, member_id, extra_fields) do
+    params = %{
+      group_urlname: group,
+      key: @api_key,
+      sign: true,
+      fields: Enum.join(extra_fields, ",")
+    }
+
+    @member_endpoint <> member_id
+    |> HTTPoison.get([], params: params )
+    |> handle_response
+  end
+
+  defp handle_response({:ok, response}) do
+    %HTTPoison.Response{status_code: 200, body: body} = response
+    Poison.decode!(body)
+  end
+
+  defp handle_response({:error, response}) do
+    {:error, "#{inspect response}"}
   end
 end
--- test/lib/group_test.exs
+++ test/lib/group_test.exs
@@ -2,17 +2,17 @@ defmodule Meetup.GroupTest do
   use ExUnit.Case, async: true
 
   test "get members with default page size" do
-    response = Meetup.Group.members("budapest-elixir")
+    members = Meetup.Group.members("budapest-elixir")
 
-    assert is_list(response)
-    assert length(response) == 20
+    assert is_list(members)
+    assert length(members) == 20
   end
 
   test "get members with page size" do
-    response = Meetup.Group.members("budapest-elixir", _page = 30)
+    members = Meetup.Group.members("budapest-elixir", _page = 30)
 
-    assert is_list(response)
-    assert length(response) == 30
+    assert is_list(members)
+    assert length(members) == 30
   end
 
   test "member has basic attributes" do
@@ -23,5 +23,16 @@ defmodule Meetup.GroupTest do
     assert "group" in attributes
     assert "member_id" in attributes
     assert "name" in attributes
+  end
+
+  test "get group member with default extra fields" do
+    member = Meetup.Group.member("budapest-elixir", "152928012")
+
+    attributes = Map.keys(member)
+
+    assert "id" in attributes
+    assert "name" in attributes
+    assert "topics" in attributes
+    assert "memberships" in attributes
   end
 end
