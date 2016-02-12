5d1f5ee90bcb65dff6bbb71ef54e790b0b1ca150
Guilherme <ggpasqualino@gmail.com>
2016-02-12 09:12:07 +0100

    Add tests for page size and member's attribute

--- test/lib/group_test.exs
+++ test/lib/group_test.exs
@@ -1,12 +1,27 @@
 defmodule Meetup.GroupTest do
   use ExUnit.Case, async: true
 
-  test "get members" do
-    response = Meetup.Group.members
-
-    IO.inspect response
+  test "get members with default page size" do
+    response = Meetup.Group.members("budapest-elixir")
 
     assert is_list(response)
     assert length(response) == 20
+  end
+
+  test "get members with page size" do
+    response = Meetup.Group.members("budapest-elixir", _page = 30)
+
+    assert is_list(response)
+    assert length(response) == 30
+  end
+
+  test "member has basic attributes" do
+    [member] = Meetup.Group.members("budapest-elixir", _page = 1)
+
+    attributes = Map.keys(member)
+
+    assert "group" in attributes
+    assert "member_id" in attributes
+    assert "name" in attributes
   end
 end
