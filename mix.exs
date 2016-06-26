189ed35f34a37da7e283ceeafb69457aff52092b
Guilherme <ggpasqualino@gmail.com>
2016-06-26 18:36:29 +0200

    Update Httpoison

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Meetup.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.8.0"},
+     {:httpoison, "~> 0.9.0"},
      {:credo, "~> 0.4.5", only: :dev},
      {:benchfella, "~> 0.3.1", only: :dev},
      {:oauth2, "~> 0.5"}]
